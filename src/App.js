7ce685d4f6d86f4bf252a52e7ee74799f50ca11f
muiruriiii <muiruricynthiaaa@gmail.com>
2023-03-02 12:43:58 +0300

    Changed to netflix favicon Login and registration page done

--- src/App.js
+++ src/App.js
@@ -3,25 +3,30 @@ import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
 import Navbar from './Navbar';
+import Login from "./Login";
+import Register from "./Register";
+
 function App() {
-  return (
-    <div className="App">
-        <Navbar/>
-        <Banner/>
-      <Row
-          title = "NETFLIX ORIGINALS"
-          fetchUrl={requests.fetchNetflixOriginals}
-          isLargeRow
-      />
-      <Row title = "Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title = "Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title = "Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title = "Documentaries" fetchUrl={requests.fetchDocumentaries} />
-    </div>
-  );
+    return (
+        <Register/>
+    );
 }
+    // <div className="App">
+    //
+    //     <Navbar/>
+    //     <Banner/>
+    //   <Row
+    //       title = "NETFLIX ORIGINALS"
+    //       fetchUrl={requests.fetchNetflixOriginals}
+    //       isLargeRow
+    //   />
+    //   <Row title = "Trending Now" fetchUrl={requests.fetchTrending} />
+    //   <Row title = "Top Rated" fetchUrl={requests.fetchTopRated} />
+    //   <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies} />
+    //   <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+    //   <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+    //   <Row title = "Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+    //   <Row title = "Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    // </div>
 
 export default App;
